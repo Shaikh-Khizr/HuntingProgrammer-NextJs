6156f86d4a277056db3a9d6d170e2b62dc088f42
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-02-24 13:16:53 +0530

    Blog jsons as data added - Tutorial 16

--- pages/blog.js
+++ pages/blog.js
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
 
+// Step 1: Collect all the files from blogdata directory
+// Step 2: Itenrate through them and display them
 const Blog = () => {
   return (
     <div className={styles.container}>
