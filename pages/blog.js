8e003139a849f7d8e38030476769b1381238743d
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-04-07 10:28:14 +0530

    Server Side Rendering Implemented - Tutorial 22

--- pages/blog.js
+++ pages/blog.js
@@ -3,18 +3,12 @@ import styles from "../styles/Blog.module.css";
 import Link from "next/link";
 
 // Step 1: Collect all the files from blogdata directory
-// Step 2: Itenrate through them and display them
+// Step 2: Iterate through them and display them
 
-const Blog = () => {
-  const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
-    console.log("useeffect is running")
-    fetch("http://localhost:3000/api/blogs").then((response) => {
-      return response.json();
-    }).then((data) => {
-      setBlogs(data);
-    })
-  }, []);
+const Blog = (props) => {
+  console.log(props);
+  const [blogs, setBlogs] = useState(props.allBlogs);
+  
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -30,5 +24,14 @@ const Blog = () => {
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+    let data = await fetch("http://localhost:3000/api/blogs")
+    let allBlogs = await data.json();
+
+  return {
+    props: { allBlogs }, // will be passed to the page component as props
+  }
+}
 
 export default Blog;
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -5,31 +5,30 @@ import styles from "../../styles/BlogPost.module.css";
 // Step 1: Find the file corresponding to the slug
 // Step 2: Populate them inside the page
 
-const slug = () => {
-  const [blog, setBlog] = useState();
-  const router = useRouter();
+const slug = (props) => {
+  const [blog, setBlog] = useState(props.myBlog);
 
-  useEffect(() => {
-    if (!router.isReady) return;
-    const { slug } = router.query;
-    fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((response) => {
-      return response.json();
-    }).then((data) => {
-      setBlog(data);
-    })
-  }, [router.isReady])
-  
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>{blog && blog.title}</h1>
         <hr />
-        <div>
-          {blog && blog.content}
-        </div>
+        <div>{blog && blog.content}</div>
       </main>
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  // console.log(context.query);
+  // const router = useRouter();
+  const { slug } = context.query;
+  let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+  let myBlog = await data.json();
+  
+  return {
+    props: {myBlog}, // will be passed to the page component as props
+  };
+}
 
 export default slug;
