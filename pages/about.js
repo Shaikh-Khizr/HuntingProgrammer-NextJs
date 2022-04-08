c7d775b74286f4f64cfe0579909ab4f1f3a0856c
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-04-08 16:15:39 +0530

    Designed the About Page - Tutorial 27

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,29 @@
 import React from "react";
+import styles from "../styles/About.module.css";
 
 const About = () => {
-  return <div>This is my about page.</div>;
+  return <div className={styles.container}>
+    <h1 className={styles.center}>About Hunting Programmer</h1>
+    <h2>Introduction</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem numquam ea soluta quidem quibusdam. Voluptate dolores doloribus error molestias ipsum, beatae eligendi recusandae libero nesciunt eos quibusdam. Voluptas fugiat delectus error! Sed omnis tenetur odio ratione tempore porro, labore dolorum alias atque mollitia necessitatibus, libero repellendus doloremque est enim. Quo soluta tempora unde consequuntur vel animi quia debitis nam quaerat impedit sint blanditiis, nisi magni, illo dolorum sit ut nobis.</p>
+    <h2>Services Offered</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nam saepe distinctio soluta eveniet ducimus amet voluptatem totam et explicabo? Debitis laboriosam tempore quae omnis at alias qui.</p>
+    <p>We offer the following services</p>
+    <ul>
+      <li>Service 1</li>
+      <li>Service 2</li>
+      <li>Service 3</li>
+      <li>Service 4</li>
+      <li>Service 5</li>
+      <li>Service 6</li>
+      <li>Service 7</li>
+      <li>Service 8</li>
+      <li>Service 9</li>
+      <li>Service 10</li>
+    </ul>
+    <h2>Contact Us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, asperiores, ab quae adipisci atque recusandae quaerat ipsam rem ullam blanditiis explicabo! Et minima voluptate veritatis magnam aut, magni architecto quos adipisci natus, fuga enim quod dolor quis saepe provident.</p>
+  </div>;
 };
 
 export default About;
